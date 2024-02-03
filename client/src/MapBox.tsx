b59bb4d153032b56589c16cb337e58e73f430bdf
Anna Wang <anna_wang@brown.edu>
2024-02-03 16:45:44 -0500

    feat: astronaut at user location

--- client/src/MapBox.tsx
+++ client/src/MapBox.tsx
@@ -1,9 +1,10 @@
-import Map, { MapLayerMouseEvent, ViewStateChangeEvent } from "react-map-gl";
-import mapboxgl from "mapbox-gl";
+import Map, {
+  MapLayerMouseEvent,
+  ViewStateChangeEvent,
+  Marker,
+} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import React, { useEffect, useState } from "react";
-
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect, useState } from "react";
 
 import { ACCESS_TOKEN } from "./private/api";
 
@@ -12,25 +13,41 @@ interface LatLong {
   long: number;
 }
 
-//wahahaha
-
 export default function MapBox() {
   const ProvidenceLatLong: LatLong = { lat: 41.824, long: -71.4128 };
-  const initialZoom = 10;
+  const initialZoom = 20;
 
+  const [userState, setUserState] = useState({
+    longitude: ProvidenceLatLong.long,
+    latitude: ProvidenceLatLong.lat,
+    zoom: initialZoom,
+  });
   const [viewState, setViewState] = useState({
     longitude: ProvidenceLatLong.long,
     latitude: ProvidenceLatLong.lat,
     zoom: initialZoom,
   });
-  // const inputRef = useRef<HTMLInputElement | null>(null);
 
   function onMapClick(e: MapLayerMouseEvent) {
     console.log(e.lngLat.lat);
     console.log(e.lngLat.lng);
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setUserState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: initialZoom,
+      });
+      setViewState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: initialZoom,
+      });
+    });
+    console.log(viewState);
+  }, []);
 
   return (
     <>
@@ -44,7 +61,16 @@ export default function MapBox() {
         style={{ width: window.innerWidth, height: window.innerHeight }}
         mapStyle={"mapbox://styles/mapbox/streets-v12"}
         onClick={onMapClick}
-      ></Map>
+      >
+        {userState && (
+          <Marker latitude={userState.latitude} longitude={userState.longitude}>
+            <img
+              src="public/astronaut-svgrepo-com.svg"
+              style={{ width: "100px" }}
+            ></img>
+          </Marker>
+        )}
+      </Map>
     </>
   );
 }
