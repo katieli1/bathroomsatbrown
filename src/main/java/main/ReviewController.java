8e5fb75de3545d122eade4050b6e8fca11a34212
kli4 <katie_li@outlook.com>
2024-02-03 16:08:32 -0500

    still sad

--- src/main/java/main/ReviewController.java
+++ src/main/java/main/ReviewController.java
@@ -1,3 +1,10 @@
 package main;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/reviews") // maps the controller to the "/posters" endpoint.
+@CrossOrigin(origins = "http://localhost:5173")
 public class ReviewController {}
