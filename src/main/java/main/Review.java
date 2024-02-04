a491e2ffdb2edd28c13690f0042c4254b0781962
kli4 <katie_li@outlook.com>
2024-02-03 19:06:29 -0500

    reviews getters/setters

--- src/main/java/main/Review.java
+++ src/main/java/main/Review.java
@@ -8,4 +8,41 @@ public class Review {
   private double overallRating;
   private double cleanlinessRating;
   private double sizeRating;
+
+  // Getter and Setter for 'review'
+  public String getReview() {
+    return review;
+  }
+
+  public void setReview(String review) {
+    this.review = review;
+  }
+
+  // Getter and Setter for 'overallRating'
+  public double getOverallRating() {
+    return overallRating;
+  }
+
+  public void setOverallRating(double overallRating) {
+    this.overallRating = overallRating;
+  }
+
+  // Getter and Setter for 'cleanlinessRating'
+  public double getCleanlinessRating() {
+    return cleanlinessRating;
+  }
+
+  public void setCleanlinessRating(double cleanlinessRating) {
+    this.cleanlinessRating = cleanlinessRating;
+  }
+
+  // Getter and Setter for 'sizeRating'
+  public double getSizeRating() {
+    return sizeRating;
+  }
+
+  public void setSizeRating(double sizeRating) {
+    this.sizeRating = sizeRating;
+  }
+
 }
